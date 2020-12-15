af381f38bc19548e2db41f11f08ac49ae4437032
Cardman <f.desrochettes@gmail.com>
2020-12-15 18:42:03 +0100

    add python samples

--- python/ex_reg_exp.py
+++ python/ex_reg_exp.py
@@ -15,7 +15,6 @@ print(Counter(words))
 print(Fichiers.lire("mots.txt").split(" "))
 print(";".join(Fichiers.lire("mots.txt").split(" ")))
 
-import re
 chaine = ""
 expression = r"^0[0-9]([ .-]?[0-9]{2}){4}$"
 print(re.search(expression, ""))
@@ -23,3 +22,24 @@ print(re.search(expression, "01-23-45-67-89"))
 print(re.search(expression, "01-23-45-6789"))
 print(re.search(expression, "01-23-45-678"))
 print(re.search(expression, "01-23-45-67890"))
+print(re.sub(r"(ab)", r" \1 ", "abcdef"))
+
+texte = """
+nom='Task1', id=8
+nom='Task2', id=31
+nom='Task3', id=127"""
+
+print(re.sub(r"id=(?P<id>[0-9]+)", r"id[\g<id>]", texte))
+#nom='Task1', id[8]
+#nom='Task2', id[31]
+#nom='Task3', id[127]
+chn_mdp = r"^[A-Za-z0-9]{6,}$"
+exp_mdp = re.compile(chn_mdp)
+print(exp_mdp.search(""))
+print(exp_mdp.search("01-23-45-67-89"))
+print(exp_mdp.search("01-23-45-6789"))
+print(exp_mdp.search("01-23-45-678"))
+print(exp_mdp.search("01-23-45-67890"))
+print(exp_mdp.search("01234567890"))
+print(exp_mdp.search("01234"))
+

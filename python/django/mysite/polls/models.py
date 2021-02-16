d483df3413c9cb903d43b835cf1edd5da130d9ad
Cardman <f.desrochettes@gmail.com>
2021-02-16 15:49:08 +0100

    improve django sample

--- python/django/mysite/polls/models.py
+++ python/django/mysite/polls/models.py
@@ -13,7 +13,9 @@ class Question(models.Model):
         return self.question_text
     def len_text(self):
         return len(self.question_text)
-
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)

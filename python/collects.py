d3053386d2578fba8ceb790f06832667637a277e
Cardman <f.desrochettes@gmail.com>
2021-03-18 14:28:31 +0100

    complete list samples

--- python/collects.py
+++ python/collects.py
@@ -1,3 +1,6 @@
+import functools#functools.cmp_to_key
+from fractions import Fraction
+
 a=[i for i in range(10)]
 liste_origine = [0, 1, 2, 3, 4, 5]
 resultat=[nb * nb for nb in liste_origine]
@@ -87,3 +90,163 @@ fonctions["premier"] = premier # on ne met pas les parenthèses
 fonctions["deuxieme"] = deuxieme
 fonctions["premier"]() # on essaye de l'appeler
 fonctions["deuxieme"]() # on essaye de l'appeler
+
+monens={0,1,2}
+print(3 in monens)#False
+print(monens.discard(3))#None
+print(1 in monens)#True
+print(monens.discard(1))#None
+#print(monens[0])#erreur
+cond=5
+maval=12 if cond > 1 else 2
+print(maval)#12
+maval=12 if cond > 10 else 2
+print(maval)#2
+
+class MaClasse1:
+    def __init__(self):
+        self.champ1=1
+
+class MaClasse2:
+    def __init__(self):
+        self.champ2=2
+
+collect_cls=[MaClasse1(),MaClasse2()]
+print(collect_cls[0].champ1)
+print(collect_cls[1].champ2)
+print()
+mult = complex(1,2)*complex(1,2)
+print(mult)
+print(mult.real)
+print(mult.imag)
+mult = complex(Fraction(1),Fraction(2))*complex(Fraction(1),Fraction(2))
+print(mult)
+print(mult.real)
+print(mult.imag)
+print()
+for e in filter(lambda x:x>2,[1,2,3,4]):
+    print(e)
+print()
+for e in map(lambda x:x*2,[1,2,3,4]):
+    print(e)
+print()
+print(functools.reduce(lambda x,y:x*y,[1,2,3,4]))
+print(functools.reduce(lambda x,y:x*y,map(lambda x:x*2,[1,2,3,4])))
+print(functools.reduce(lambda x,y:x*y,map(lambda x:x*2,filter(lambda x:x>2,[1,2,3,4]))))
+print()
+for e in map(lambda x,y:x*y*2,[1,2,3,4],[11,12,13,14]):
+    print(e)
+print()
+
+class MaCmp:
+    def __init__(self,nb):
+        self.macmp=nb
+    @staticmethod
+    def utilcmp(val):
+        return val.macmp
+    @staticmethod
+    def utilcmpopp(val):
+        return -val.macmp
+    @staticmethod
+    def utilcmptwo(val,val2):
+        return val.macmp-val2.macmp
+    @staticmethod
+    def utilcmpopptwo(val,val2):
+        return val2.macmp-val.macmp
+    def __repr__(self):#dev, utilise par les listes
+        return str(MaCmp.utilcmp(self))
+    def __str__(self):#client
+        return str(self.macmp)
+
+print(max([1,2,3,4]))
+print(max(1,2,3,4))
+print(max([MaCmp(1),MaCmp(2),MaCmp(3),MaCmp(4)],key=lambda x:x.macmp)," >< ",\
+    max([MaCmp(1),MaCmp(2),MaCmp(3),MaCmp(4)],key=lambda x:-x.macmp))
+print(max([],key=lambda x:x.macmp,default=MaCmp(5))," >< ",\
+    max([],key=lambda x:-x.macmp,default=MaCmp(5)))
+print(max(MaCmp(1),MaCmp(2),MaCmp(3),MaCmp(4),key=lambda x:x.macmp)," >< ",\
+    max(MaCmp(1),MaCmp(2),MaCmp(3),MaCmp(4),key=lambda x:-x.macmp))
+print()
+print(max([MaCmp(1),MaCmp(2),MaCmp(3),MaCmp(4)],key=MaCmp.utilcmp)," >< ",\
+    max([MaCmp(1),MaCmp(2),MaCmp(3),MaCmp(4)],key=MaCmp.utilcmpopp))
+print(max([],key=lambda x:x.macmp,default=MaCmp(5))," >< ",\
+    max([],key=lambda x:-x.macmp,default=MaCmp(5)))
+print(max(MaCmp(1),MaCmp(2),MaCmp(3),MaCmp(4),key=MaCmp.utilcmp)," >< ",\
+    max(MaCmp(1),MaCmp(2),MaCmp(3),MaCmp(4),key=MaCmp.utilcmpopp))
+print()
+print(min([1,2,3,4]))
+print(min(1,2,3,4))
+print(min([MaCmp(1),MaCmp(2),MaCmp(3),MaCmp(4)],key=lambda x:x.macmp)," >< ",\
+    min([MaCmp(1),MaCmp(2),MaCmp(3),MaCmp(4)],key=lambda x:-x.macmp))
+print(min([],key=lambda x:x.macmp,default=MaCmp(5))," >< ",\
+    min([],key=lambda x:-x.macmp,default=MaCmp(5)))
+print(min(MaCmp(1),MaCmp(2),MaCmp(3),MaCmp(4),key=lambda x:x.macmp)," >< ",\
+    min(MaCmp(1),MaCmp(2),MaCmp(3),MaCmp(4),key=lambda x:-x.macmp))
+print()
+print(min([MaCmp(1),MaCmp(2),MaCmp(3),MaCmp(4)],key=MaCmp.utilcmp)," >< ",\
+    min([MaCmp(1),MaCmp(2),MaCmp(3),MaCmp(4)],key=MaCmp.utilcmpopp))
+print(min([],key=lambda x:x.macmp,default=MaCmp(5))," >< ",\
+    min([],key=lambda x:-x.macmp,default=MaCmp(5)))
+print(min(MaCmp(1),MaCmp(2),MaCmp(3),MaCmp(4),key=MaCmp.utilcmp)," >< ",\
+    min(MaCmp(1),MaCmp(2),MaCmp(3),MaCmp(4),key=MaCmp.utilcmpopp))
+print()
+print()
+
+class MaCollec:
+    def __init__(self,ls):
+        self.ls=ls
+    def __iter__(self):
+        return MonIter(self.ls)
+    def __reversed__(self):
+        return MonIterInv(self.ls)
+class MonIter:
+    def __init__(self,ls):
+        self.ls=ls
+        self.ind=0
+    def __iter__(self):
+        return self
+    def __next__(self):
+        if self.ind >= len(self.ls):
+            raise StopIteration()
+        val = self.ls[self.ind]
+        self.ind+=1
+        return val
+class MonIterInv:
+    def __init__(self,ls):
+        self.ls=ls
+        self.ind=-1
+    def __iter__(self):
+        return self
+    def __next__(self):
+        if self.ind < -len(self.ls):
+            raise StopIteration()
+        val = self.ls[self.ind]
+        self.ind-=1
+        return val
+
+perso_iter = MaCollec([1,2,3,4])
+for i in perso_iter:
+    print(i)
+print()
+for i in reversed(perso_iter):
+    print(i)
+print()
+print()
+print(sorted([MaCmp(4),MaCmp(2),MaCmp(3),MaCmp(1)],key=lambda x:x.macmp)," >< ",\
+    sorted([MaCmp(4),MaCmp(2),MaCmp(3),MaCmp(1)],key=lambda x:-x.macmp))
+print(sorted([MaCmp(1),MaCmp(2),MaCmp(3),MaCmp(4)],key=MaCmp.utilcmp)," >< ",\
+    sorted([MaCmp(1),MaCmp(2),MaCmp(3),MaCmp(4)],key=MaCmp.utilcmpopp))
+print(sorted([MaCmp(1),MaCmp(2),MaCmp(3),MaCmp(4)],key=functools.cmp_to_key(MaCmp.utilcmptwo))," >< ",\
+    sorted([MaCmp(1),MaCmp(2),MaCmp(3),MaCmp(4)],key=functools.cmp_to_key(MaCmp.utilcmpopptwo)))
+
+for i,x in enumerate([15,25,32,41]):
+    print(i,",",x)
+liste_paires = [(i, x) for i,x in enumerate([15,25,32,41])]
+for p in liste_paires:
+    print(p[0],",",p[1])
+
+for p in [(i, x) for i,x in enumerate([15,25,32,41])]:
+    print(p[0],",",p[1])
+
+print()
+print(MaCmp.utilcmp(val=MaCmp(nb=12)))

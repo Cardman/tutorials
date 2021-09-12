fed6620fb6639b9f265c1ca72aef42081399f9dd
Cardman <f.desrochettes@gmail.com>
2021-09-12 12:10:31 +0200

    improve recorder

--- java_audio_recorder/src/main/java/code/recorder/CustSoundRecorder.java
+++ java_audio_recorder/src/main/java/code/recorder/CustSoundRecorder.java
@@ -95,6 +95,31 @@ public class CustSoundRecorder {
         // start recording
         return startRecord(args[0],format,info);
     }
+    static Mixer.Info[] mixers()throws Exception {
+		
+		Mixer.Info[] mixerInfos = AudioSystem.getMixerInfo();
+		 for (Mixer.Info info: mixerInfos){
+			  Mixer m = AudioSystem.getMixer(info);
+			  Line.Info[] lineInfos = m.getSourceLineInfo();
+			  for (Line.Info lineInfo:lineInfos){
+				   System.out.println (info.getName()+"-<=--"+lineInfo);
+				   Line line = m.getLine(lineInfo);
+				   System.out.println("\t---<=--"+line);
+			  }
+			  lineInfos = m.getTargetLineInfo();
+			  for (Line.Info lineInfo:lineInfos){
+				   System.out.println (m+"-=>--"+lineInfo);
+				   Line line = m.getLine(lineInfo);
+				   System.out.println("\t--=>---"+line);
+
+			  }
+
+		 }
+		 AudioFormat format = new AudioFormat(8000.0f, 16, 1, true, true);
+		TargetDataLine microphone = AudioSystem.getTargetDataLine(format);
+		System.out.println("\t----%%%-"+microphone);
+			return mixerInfos;
+	}
 }
 
 class WaitForEnd implements Runnable {

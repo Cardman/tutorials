c372203f1915bb9b7cfef79896bf76c19c4ffeb1
Cardman <f.desrochettes@gmail.com>
2021-09-19 12:21:53 +0200

    complete sample

--- spring_el2/src/main/java/__/__.java
+++ spring_el2/src/main/java/__/__.java
@@ -3,8 +3,10 @@ package __;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"code"})
 public class __{
 
 	public static void main(String[] args) {

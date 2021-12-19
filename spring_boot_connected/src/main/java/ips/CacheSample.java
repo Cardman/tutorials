d2370e94eb489a9bb8704310e3ef7326d5b21db9
Cardman <f.desrochettes@gmail.com>
2021-12-19 15:55:25 +0100

    complete cache sample

--- spring_boot_connected/src/main/java/ips/CacheSample.java
+++ spring_boot_connected/src/main/java/ips/CacheSample.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.*;
 @Service
 public class CacheSample{
 	private AtomicLong count = new AtomicLong(0);
+	private AtomicLong count2 = new AtomicLong(0);
+	private AtomicLong count3 = new AtomicLong(0);
 	@Cacheable(value = "first", key = "#cacheKey")
 	public long addSingleCacheValue(String cacheKey) {
 		count.incrementAndGet();
@@ -69,7 +71,133 @@ public class CacheSample{
 		count.set(0);
 		return cacheKey.length();
 	}
+	@Cacheable(value = "second")
+	public long addSingleCacheValue(String cacheKey, int counter) {
+		count2.incrementAndGet();
+		long sum = 0L;
+		for (char c: cacheKey.toCharArray()){
+			count2.incrementAndGet();
+			int type = StringDataUtil.getCustomType(c);
+			if (type == 1){
+				sum += c * 2;
+				continue;
+			}
+			if (type == 2){
+				sum += c * 5;
+				continue;
+			}
+			if (type == 3 || type == 4){
+				sum += c + 1;
+				continue;
+			}
+			if (type == 5 || type == 6){
+				sum += c * 4 + 1;
+				continue;
+			}
+			if (type == 7 || type == 8 || type == 9){
+				sum += c * 2 + 3;
+				continue;
+			}
+			if (type == 10){
+				sum += (c-'0'+1) * (c*3-'0'+2);
+				continue;
+			}
+			if (type == 11){
+				sum += c-'0'-15*16;
+				continue;
+			}
+			if (type >= 12 && type <= 15){
+				sum += 15*c-15*16*1500;
+				continue;
+			}
+			if (type >= 16 && type <= 30){
+				sum += c * c * c /(c * c + 1);
+				continue;
+			}
+			if (type == 31){
+				sum += c * c - c * 15000;
+				continue;
+			}
+			if (type == 32){
+				sum += c * c / 4 - 1235;
+				continue;
+			}
+			sum += c * c * c - c * c * 5 + 12 * c -c;
+		}
+		return sum * counter;
+	}
+	@CacheEvict(value = "second")
+	public int evictSingleCacheValue(String cacheKey, int counter) {
+		count2.set(0);
+		return cacheKey.length();
+	}
+	@Cacheable(value = "third", key = "#cacheKey+\"_\"+#counter")
+	public long addSingleCacheValue1(String cacheKey, int counter) {
+		count3.incrementAndGet();
+		long sum = 0L;
+		for (char c: cacheKey.toCharArray()){
+			count3.incrementAndGet();
+			int type = StringDataUtil.getCustomType(c);
+			if (type == 1){
+				sum += c * 2 * c;
+				continue;
+			}
+			if (type == 2){
+				sum += c * 5 * c;
+				continue;
+			}
+			if (type == 3 || type == 4){
+				sum += c + 1;
+				continue;
+			}
+			if (type == 5 || type == 6){
+				sum += c * 4 + 1;
+				continue;
+			}
+			if (type == 7 || type == 8 || type == 9){
+				sum += c * 2 + 3;
+				continue;
+			}
+			if (type == 10){
+				sum += (c-'0'+1) * (c*3-'0'+2);
+				continue;
+			}
+			if (type == 11){
+				sum += c-'0'-15*16;
+				continue;
+			}
+			if (type >= 12 && type <= 15){
+				sum += 15*c-15*16*1500;
+				continue;
+			}
+			if (type >= 16 && type <= 30){
+				sum += c * c * c /(c * c + 1);
+				continue;
+			}
+			if (type == 31){
+				sum += c * c - c * 15000;
+				continue;
+			}
+			if (type == 32){
+				sum += c * c / 4 - 1235;
+				continue;
+			}
+			sum += c * c * c - c * c * 5 + 12 * c -c;
+		}
+		return sum * counter;
+	}
+	@CacheEvict(value = "third", key = "#cacheKey+\"_\"+#counter")
+	public int evictSingleCacheValue1(String cacheKey, int counter) {
+		count3.set(0);
+		return cacheKey.length();
+	}
 	public long get(){
 		return count.get();
+	}
+	public long get2(){
+		return count2.get();
+	}
+	public long get3(){
+		return count3.get();
 	}
 }
--- spring_boot_connected/src/main/java/ips/IpController.java
+++ spring_boot_connected/src/main/java/ips/IpController.java
@@ -23,11 +23,31 @@ public class IpController{
 	@GetMapping(path = {"/add"})
     public String add(@RequestParam("key") String key){
         long value = cacheSample.addSingleCacheValue(key);
-        return key+":"+value+","+cacheSample.get();
+        return key+"+:"+value+","+cacheSample.get();
     }
 	@GetMapping(path = {"/remove"})
     public String remove(@RequestParam("key") String key){
         int value = cacheSample.evictSingleCacheValue(key);
-        return key+":"+value+","+cacheSample.get();
+        return key+"-:"+value+","+cacheSample.get();
+    }
+	@GetMapping(path = {"/add2"})
+    public String add(@RequestParam("key") String key,@RequestParam("counter") int counter){
+        long value = cacheSample.addSingleCacheValue(key,counter);
+        return key+"++:"+value+","+cacheSample.get2();
+    }
+	@GetMapping(path = {"/remove2"})
+    public String remove(@RequestParam("key") String key,@RequestParam("counter") int counter){
+        int value = cacheSample.evictSingleCacheValue(key,counter);
+        return key+"--:"+value+","+cacheSample.get2();
+    }
+	@GetMapping(path = {"/add3"})
+    public String add2(@RequestParam("key") String key,@RequestParam("counter") int counter){
+        long value = cacheSample.addSingleCacheValue1(key,counter);
+        return key+"+++:"+value+","+cacheSample.get3();
+    }
+	@GetMapping(path = {"/remove3"})
+    public String remove2(@RequestParam("key") String key,@RequestParam("counter") int counter){
+        int value = cacheSample.evictSingleCacheValue1(key,counter);
+        return key+"---:"+value+","+cacheSample.get3();
     }
 }

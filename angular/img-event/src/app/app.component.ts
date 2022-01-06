ee312abf21833f6465a55f62ba018a4b9bbf8a43
Cardman <f.desrochettes@gmail.com>
2022-01-06 10:25:21 +0100

    add img event sample

---
+++ angular/img-event/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'img-event';
+  state = -1;
+  onClick($event:number){
+	this.state = $event;
+  }
+  
+}

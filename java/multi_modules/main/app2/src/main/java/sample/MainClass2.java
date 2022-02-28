c8edeb64c09ebdd2e9d947185f558511064174ef
Cardman <f.desrochettes@gmail.com>
2022-02-28 18:32:49 +0100

    complete sample

--- java/multi_modules/main/app2/src/main/java/sample/MainClass2.java
+++ java/multi_modules/main/app2/src/main/java/sample/MainClass2.java
@@ -1,7 +1,13 @@
 package sample;
 
+import javax.swing.*;
+
 public class MainClass2{
 	public static void main(String...args){
-		
+		JFrame fr = new JFrame();
+		fr.setContentPane(new JLabel(System.getProperty("java.version")+",2"));
+		fr.pack();
+		fr.setVisible(true);
+		fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 }

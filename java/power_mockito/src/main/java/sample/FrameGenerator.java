339d6d3fcfcee2c72364a4100cd82abb3d59ba78
Cardman <f.desrochettes@gmail.com>
2022-09-18 11:51:29 +0200

    add mock sample

--- java/power_mockito/src/main/java/sample/FrameGenerator.java
+++ java/power_mockito/src/main/java/sample/FrameGenerator.java
@@ -1,9 +1,17 @@
 package sample;
 
+import java.awt.*;
+import java.awt.image.MemoryImageSource;
 import javax.swing.*;
 
 public final class FrameGenerator{
+    private static final String SELECT = "select";
 	public JFrame frame(){
 		return new JFrame();
+	}
+	public Cursor cursor(int _wCurs, int _hCurs, int[] _pixels){
+        Toolkit tool_ = Toolkit.getDefaultToolkit();
+        Image b_ = tool_.createImage(new MemoryImageSource(_wCurs, _hCurs, _pixels, 0, _wCurs));
+        return tool_.createCustomCursor(b_, new Point(0, 0),SELECT);
 	}
 }
--- java/power_mockito/src/test/java/sample/FrameGeneratorTest.java
+++ java/power_mockito/src/test/java/sample/FrameGeneratorTest.java
@@ -1,5 +1,7 @@
 package sample;
 
+import java.awt.*;
+import java.awt.image.MemoryImageSource;
 import javax.swing.*;
 import org.junit.*;
 import org.junit.runner.RunWith;
@@ -14,7 +16,7 @@ import static org.powermock.api.mockito.PowerMockito.*;
 import org.powermock.api.mockito.PowerMockito;
 
 @RunWith(PowerMockRunner.class)
-@PowerMockIgnore("javax.swing.*")
+@PowerMockIgnore({"javax.swing.*","java.awt.*","java.awt.image.*"})
 public final class FrameGeneratorTest{
 
 
@@ -25,6 +27,20 @@ public final class FrameGeneratorTest{
         whenNew(JFrame.class).withNoArguments().thenReturn(mock);
 		FrameGenerator frameGene = new FrameGenerator();
 		Assert.assertNotNull(frameGene.frame());
+    }
+    @Test
+    public void cursor() throws Exception {
+		PowerMockito.mockStatic(Toolkit.class);
+		MemoryImageSource mockMemoryImageSource = mock(MemoryImageSource.class);
+		Toolkit mock = mock(Toolkit.class);
+		//
+		//whenNew(MemoryImageSource.class).withNoArguments().thenReturn(mockMemoryImageSource);
+		whenNew(MemoryImageSource.class).withAnyArguments().thenReturn(mockMemoryImageSource);
+		when(Toolkit.getDefaultToolkit()).thenReturn(mock);
+		doReturn(mock(Image.class)).when(mock).createImage(Mockito.any(MemoryImageSource.class));
+		doReturn(mock(Cursor.class)).when(mock).createCustomCursor((Image)Mockito.any(),(Point)Mockito.any(),(String)Mockito.any());
+        FrameGenerator frameGene = new FrameGenerator();
+		Assert.assertNotNull(frameGene.cursor(0,0,new int[0]));
     }
 
 }

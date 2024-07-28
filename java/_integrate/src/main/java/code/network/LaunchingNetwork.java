8b025f2e4452df2496f689172e4d6ed5ca467e81
Cardman <f.desrochettes@gmail.com>
2024-07-28 09:51:16 +0200

    change poc about some gui applications

--- java/_integrate/src/main/java/code/network/LaunchingNetwork.java
+++ java/_integrate/src/main/java/code/network/LaunchingNetwork.java
@@ -1,37 +1,16 @@
 package code.network;
 
-import aiki.main.AikiFactory;
-import cards.main.CardFactories;
-import code.gui.AbsButton;
-import code.gui.AdvSoftApplicationCore;
-import code.gui.initialize.AbstractProgramInfos;
-import code.threads.AbstractBaseExecutorService;
+import code.gui.*;
 
 public final class LaunchingNetwork extends AdvSoftApplicationCore {
 
-    private final CardFactories cardFactories;
-    private final AikiFactory aikiFactory;
-    private final AbsButton button;
-    private final AbstractBaseExecutorService serv;
-    public LaunchingNetwork(AbstractProgramInfos _frames, CardFactories _c, AikiFactory _a, AbsButton _b, AbstractBaseExecutorService _service) {
+    public LaunchingNetwork(WithAppFactories _frames) {
         super(_frames);
-        cardFactories = _c;
-        aikiFactory = _a;
-        button = _b;
-        serv = _service;
     }
 
     @Override
-    protected String getApplicationName() {
-        return getMainWindowClass();
+    protected void launch(String _language, InterpretedFile _args, EnabledMenu _lgMenu, AbsButton _main) {
+        getFrames().getCompoFactory().invokeNow(new LaunchNetwork(_language,getFrames(), _main,_lgMenu,getAppFactories().getAikiFactory(),getAppFactories().getCardFactories()));
     }
 
-    @Override
-    protected void launch(String _language, String[] _args) {
-        serv.execute(new LaunchNetwork(_language,getFrames(),cardFactories,aikiFactory,button));
-    }
-
-    public static String getMainWindowClass() {
-        return "network";
-    }
 }
